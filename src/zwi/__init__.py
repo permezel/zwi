60b2d00979876cd5fae05e38bf1bc95f4d3f32e3
Damon Permezel <permezel@me.com>
2021-08-05 12:35:04 +1000

    bump release: 0.3 alpha2 development 0

--- src/zwi/__init__.py
+++ src/zwi/__init__.py
@@ -11,7 +11,7 @@ from .core import *
 from .asset_cache import *
 from .qt_gui import *
 
-__version__ = '0.3a1'
+__version__ = '0.3a2.dev0'
 
 __all__ = [name for name, obj in locals().items()
            if not (name.startswith('_') or inspect.ismodule(obj))]
