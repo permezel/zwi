c7c12b57f21c738e56a15f819bbb8b17e45d8639
Damon Permezel <dap@zepherin.com>
2021-09-11 18:18:40 +1000

    Bumb version.

--- src/zwi/__init__.py
+++ src/zwi/__init__.py
@@ -11,7 +11,7 @@ from .core import *
 from .asset_cache import *
 from .qt_gui import *
 
-__version__ = '0.3a2.dev1'
+__version__ = '0.3a2.dev2'
 
 __all__ = [name for name, obj in locals().items()
            if not (name.startswith('_') or inspect.ismodule(obj))]
